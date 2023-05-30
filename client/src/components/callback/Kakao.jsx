9c50c8a9b5d0c53307baa1f2df5b8d2e5116c749
Youn Byung Hyuk <ybh0311@naver.com>
2023-05-30 19:20:55 +0900

    env 파일 추가

--- client/src/components/callback/Kakao.jsx
+++ client/src/components/callback/Kakao.jsx
@@ -1,4 +1,3 @@
-
 import axios from "axios";
 import React, { useContext, useEffect } from "react";
 import { createContext } from "react";
@@ -9,30 +8,29 @@ export default function Kakao() {
   const { accessToken, setAccessToken, loginType, setLoginType } =
     useContext(UserContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      let params = new URL(document.location.toString()).searchParams;
-      let code = params.get("code"); // 인가코드 받는 부분
-      let grant_type = "authorization_code";
-      let client_id = "b95846f7ffaa2715ee237dd29dbcd482";
-  
-      axios.post(`https://kauth.kakao.com/oauth/token?
-          grant_type=${grant_type}
-          &client_id=${client_id}
-          &redirect_uri=http://localhost:3000/oauth/callback/kakao
-          &code=${code}`
-          , {
-      headers: {
-          'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-      }
-    }).then((res) => {
-        console.log(res)
-        // res에 포함된 토큰 받아서 원하는 로직을 하면된다.
-    })
-    }, []);  
+  useEffect(() => {
+    try {
+    const url = new URL(window.location.href);
+    const code = url.searchParams.get("code");
+    if (code) {
+      axios({
+        url: "http://localhost:8112/auth/accesstoken",
+        method: "post",
+        data: {
+          code: code,
+        },
+      }).then((result) => {
+        const accessToken = result.data.split("=")[1].split("&")[0];
+        setAccessToken(accessToken);
+        setLoginType("KAKAO");
+        navigate("/");
+      });
+    }
+  } catch (error) {}
 
-  return (
-    <div>Kakao</div>
-  )
+  }, []);
+
+  return <div>KAKAO Callback</div>;
 }
