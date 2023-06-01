ad5a7ebe843ec059f1f805d7238cf829ccda796c
minwg1021 <minwg1021@gmail.com>
2023-06-01 23:12:11 +0900

    tm.html - 스쿼트 올라갈 때 카운트 추가로 변경

--- client/src/pages/main/MainPage.js
+++ client/src/pages/main/MainPage.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./MainPage.css";
-import Footer from "../Footer";
-import Header from "../Header";
-import Main from "./Main";
+import Footer from "../footer";
+import Header from "../header";
+import Main from "./main";
 
 function MainPage() {
   return (
     <React.Fragment>
       <div className="main-page">
         <Header />
-        <Main/>
+        <Main />
         <Footer />
       </div>
     </React.Fragment>
