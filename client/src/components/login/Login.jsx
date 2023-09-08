9ac322ffa8b45391c9a91a9263bef8f2294e86c8
Youn Byung Hyuk <ybh0311@naver.com>
2023-09-08 14:41:44 +0900

    로그인 구현 및 mysql 연동

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,45 +1,52 @@
-import axios from "axios";
-import React, { useContext, useEffect } from "react";
+import React from "react";
+import { useState } from "react";
 import "./login.css";
+import axios from "axios";
 
-export default function Login() {
-  const kakaoid = "b95846f7ffaa2715ee237dd29dbcd482";
-  const kakaoREDIRECT_URI = "http://localhost:3000/auth/callback/kakao";
-  const github = () => {
-    window
-      .open(
-        "https://github.com/login/oauth/authorize" +
-          `?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}`,
-        "_self"
-      )
+export default function Login({ setIsLogin, setUser }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = () => {
+    axios({
+      url: "http://localhost:8123/login",
+      method: "POST",
+      withCredentials: true,
+      data: {
+        email: email,
+        password: password,
+      },
+    }).then((result) => {
+      if (result.status === 200) {
+        window.open('/', '_self')
+      }
+    });
   };
-
-  const google = () => {
-
-  }
-
-  const kakao = () => {
-    window
-    .open(
-      `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoid}&redirect_uri=${kakaoREDIRECT_URI}&response_type=code`
-    )
-
-  }
 
   return (
     <div>
       <div className="loginContainer">
         <div className="inputGroup">
-          <button className="loginButton" onClick={github}>
-            <img src="../assets/git.jpg" alt="" className="buttonImg" />
-          </button>
-          <button className="loginButton" onClick={google}>
-            <img src="../assets/google.png" alt="" className="buttonImg" />
-          </button>
-          <button className="loginButton" onClick={kakao}>
-            <img src="../assets/kakao.png" alt="" className="buttonImg" />
-          </button>
+          <label className="inputLabel">email</label>
+          <input
+            type="email"
+            placeholder="email"
+            className="inputValue"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
         </div>
+        <div className="inputGroup">
+          <label className="inputLabel">password</label>
+          <input
+            type="password"
+            placeholder="password"
+            className="inputValue"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+        <button onClick={login} className="loginButton">Login</button>
       </div>
     </div>
   );
--- server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { accessToken, callback } = require('../controller')
-
-router.post('/accesstoken', accessToken);
-
-module.exports = router;
