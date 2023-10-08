365eec365c4a4ffd327273e113597cf97b1801ff
Youn Byung Hyuk <ybh0311@naver.com>
2023-10-08 17:12:18 +0900

    운동별 데이터 조회 로직 구현

--- server/view/index.js
+++ server/view/index.js
@@ -131,6 +131,78 @@ const view_squat_daily = (req, res) => {
   }
 };
 
+const view_crunch_daily = (req, res) => {
+  try {
+    const token = req.cookies.accessToken; // req의 쿠키에서 엑세스 토큰에 접근
+    const data = jwt.verify(token, process.env.ACCESS_SECRET); // verify 해줌
+    db.query(
+      "SELECT DATE(`date`) AS day, SUM(`counts`) AS total_count FROM `side_crunch` WHERE `user` = ? GROUP BY day ORDER BY day;",
+      [data.id],
+      // 콜백함수
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(401).json("불러오기 실패");
+        } else {
+          console.log("불러오기 성공");
+          console.log(result);
+          res.status(200).json(result);
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const view_shoulder_daily = (req, res) => {
+  try {
+    const token = req.cookies.accessToken; // req의 쿠키에서 엑세스 토큰에 접근
+    const data = jwt.verify(token, process.env.ACCESS_SECRET); // verify 해줌
+    db.query(
+      "SELECT DATE(`date`) AS day, SUM(`counts`) AS total_count FROM `shoulder_press` WHERE `user` = ? GROUP BY day ORDER BY day;",
+      [data.id],
+      // 콜백함수
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(401).json("불러오기 실패");
+        } else {
+          console.log("불러오기 성공");
+          console.log(result);
+          res.status(200).json(result);
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const view_lateral_raise_daily = (req, res) => {
+  try {
+    const token = req.cookies.accessToken; // req의 쿠키에서 엑세스 토큰에 접근
+    const data = jwt.verify(token, process.env.ACCESS_SECRET); // verify 해줌
+    db.query(
+      "SELECT DATE(`date`) AS day, SUM(`counts`) AS total_count FROM `side_lateral_raise` WHERE `user` = ? GROUP BY day ORDER BY day;",
+      [data.id],
+      // 콜백함수
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(401).json("불러오기 실패");
+        } else {
+          console.log("불러오기 성공");
+          console.log(result);
+          res.status(200).json(result);
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
   // 작성된 함수를 모듈화해서 내보냄
   view_squat_month,
