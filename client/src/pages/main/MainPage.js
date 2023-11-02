8baf5272cfded0a7648d9e3de4eb5fbfe7e5fde9
Baek <woonn97@gmail.com>
2023-11-02 19:52:05 +0900

    fix: CSS수정

--- client/src/pages/main/MainPage.js
+++ client/src/pages/main/MainPage.js
@@ -1,24 +1,85 @@
 import React from "react";
-import "./MainPage.css";
 import Footer from "../footer";
 import Header from "../header";
 import { useNavigate } from "react-router-dom";
 import startWorkOut from "./startWorkOut.gif";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 function MainPage() {
-  let Navigate = useNavigate();
+  let navigate = useNavigate();
+
+  const StartClick = () => {
+    navigate("/ExerciseChoice");
+  };
+
   return (
     <>
       <Header />
-      <main className="main-main">
-        <a className="btn" onClick={() => Navigate("/ExerciseChoice")}>
-          <img src={startWorkOut} alt="startWorkOut"></img>
-        </a>
-        <img className="main_background" src="main.jpg"></img>
-      </main>
+      <MainContainer>
+        <SectionOne>
+          <StartImage2 src="main_logo2.png" />
+          <StartButton onClick={StartClick}>운동 시작하기</StartButton>
+        </SectionOne>
+        <SectionTwo></SectionTwo>
+      </MainContainer>
       <Footer />
     </>
   );
 }
+
+const SectionOne = styled.section`
+  width: 100%;
+  height: max-content;
+  display: flex;
+  flex-direction: Column;
+  margin-top: 20vh;
+  justify-content: center;
+  align-items: center;
+`;
+const SectionTwo = styled.section`
+  width: 100vw;
+  height: calc(100vh);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 80px;
+  background: linear-gradient(${(props) => props.theme.light}, white);
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column; /* 위에서 아래로 정렬 (열) */
+  justify-content: center; /*세로방향 가운데 정렬*/
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
+const StartButton = styled.button`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #555;
+  border-radius: 100px;
+  font-weight: 900;
+  background: transparent;
+  font-size: 26px;
+  color: #555;
+  padding: 13px 24px;
+  font-family: "Spoqa Han Sans Neo";
+
+  &:hover {
+    cursor: pointer;
+    background: #a4c4ee;
+    color: #ffffff;
+    border: 2px solid #ffffff;
+  }
+`;
+const StartImage2 = styled.img`
+  width: 45%;
+  padding-left: 250px;
+  padding-bottom: 100px;
+`;
 
 export default MainPage;
